796e3afcbb574f955041e11b56e134c2efecce8a
VirtualShip <74993299+VirtualShip@users.noreply.github.com>
2020-12-08 16:23:50 -0800

    Update a52dec_install.sh

--- stock/a52dec_install.sh
+++ stock/a52dec_install.sh
@@ -1,10 +1,10 @@
-a52dec_install () {
+52dec_install () {
 	cd /usr/local/Garage
 	echo -e "\033[34;1;4m🖥️  Processing order: a52dec \033[0m"
 	sleep 2
 	echo -e "\033[93;1;4m🏗  Retrieving components of order \033[0m"
 	sleep 2
-	gdown https://drive.google.com/uc?id=1zQ4AXFC9Gcz0eqIuWLrkzmKQXubkeKhm
+	gdown https://drive.google.com/uc?id=1GH1JZpdx7W2eh66s56FSi14FbCQZvcEB
 	echo -e "\033[33;1;4m📦 Packaging components of order... \033[0m"
 	sleep 2
 	unzip a52dec.zip
