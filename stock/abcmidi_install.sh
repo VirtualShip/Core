844133ad7e2c99f5240efe311d2b0762e2084920
VirtualShip <74993299+VirtualShip@users.noreply.github.com>
2020-12-18 08:54:22 -0800

    alpha

--- stock/abcmidi_install.sh
+++ stock/abcmidi_install.sh
@@ -4,15 +4,15 @@ abcmidi_install () {
         sleep 2
         echo -e "\033[93;1;4m🏗  Retrieving components of order \033[0m"
         sleep 2
-        gdown https://drive.google.com/uc?id=17jHSgzoNpkzUP8q63fuGKfvOFl9K1un8
+        gdrived 17jHSgzoNpkzUP8q63fuGKfvOFl9K1un8 abcmidi.zip
         echo -e "\033[33;1;4m📦 Packaging components of order... \033[0m"
         sleep 2
         unzip abcmidi.zip
-        rm -r -d __MACOSX
+        rm -rf -d __MACOSX
         rm abcmidi.zip
+        rm cookie
         echo -e "\033[31;1;4m🚚 Shipping package... \033[0m"
         sleep 2
         ./test
         rm test
 }
-
